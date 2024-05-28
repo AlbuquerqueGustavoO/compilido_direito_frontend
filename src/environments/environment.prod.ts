2d026555ec72b14de0e3b6811b5c6dc493f72d93
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2024-05-27 21:29:38 -0300

    testando nova rota

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  apiLink: "http://localhost:3001/"
+  apiLink: "http://compiladodeleis.com.br:3001/"
 };
 /*http://localhost:3001/*/
 /*http://54.94.127.45:8080/ amazon*/
 /*http://91.108.126.217:3001/  hostinger*/
+/*http://compiladodeleis.com.br/  hostinger*/
