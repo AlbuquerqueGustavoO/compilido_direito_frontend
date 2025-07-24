0bbc046c139866556748045093505f131e5829cc
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2025-07-24 17:36:16 -0300

    ajuste environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiLink: "http://localhost:3001/"
+  apiLink: "https://compiladodeleis.com.br:3001"
 };
 /*http://localhost:3001/*/
 /*http://54.94.127.45:8080/ amazon*/
