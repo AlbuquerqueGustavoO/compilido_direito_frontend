79264d5d3a93d188cf29777db8b37cb3b84a9350
Gustavo Albuquerque <gustavoo_albuquerque@hotmail.com>
2025-07-24 17:54:45 -0300

    teste CI/CD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  apiLink: "https://compiladodeleis.com.br:3001"
+  apiLink: "https://compiladodeleis.com.br:3001/"
 };
 /*http://localhost:3001/*/
-/*http://54.94.127.45:8080/ amazon*/
 /*http://91.108.126.217:3001/  hostinger*/
 /*https://compiladodeleis.com.br:3001/  hostinger*/
